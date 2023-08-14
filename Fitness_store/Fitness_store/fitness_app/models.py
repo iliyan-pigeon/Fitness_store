f38ee217b69ea0400a7d8d241f6c8bb972317483
Iliyan Galabov <milenov556@gmail.com>
2023-08-14 16:13:36 +0300

    First models added

--- Fitness_store/Fitness_store/fitness_app/models.py
+++ Fitness_store/Fitness_store/fitness_app/models.py
@@ -1,3 +1,56 @@
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class BestSellingSupplements(models.Model):
+
+    name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=30,
+        validators=(
+            validators.MinLengthValidator(2),
+        ),
+    )
+
+    description = models.TextField(
+        null=False,
+        blank=False,
+        max_length=300,
+        validators=(
+            validators.MinLengthValidator(5),
+        ),
+    )
+
+    photo = models.FileField(
+        null=False,
+        blank=False,
+        upload_to='images/'
+    )
+
+
+class BestSellingGymEquipment(models.Model):
+
+    name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=30,
+        validators=(
+            validators.MinLengthValidator(2),
+        ),
+    )
+
+    description = models.TextField(
+        null=False,
+        blank=False,
+        max_length=300,
+        validators=(
+            validators.MinLengthValidator(5),
+        ),
+    )
+
+    photo = models.FileField(
+        null=False,
+        blank=False,
+        upload_to='images/'
+    )
