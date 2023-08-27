3d1699a53f3c82b4128c43b3869651cd9948b05a
Iliyan Galabov <milenov556@gmail.com>
2023-08-27 21:33:28 +0300

    Added 'next' to the register template and View. Changed RegisterForm.

--- Fitness_store/Fitness_store/fitness_app/forms.py
+++ Fitness_store/Fitness_store/fitness_app/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import forms as auth_forms, get_user_model
 from django.contrib.auth.models import User
 
 
-class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
+UserModel = get_user_model()
+
+
+class RegisterUserForm(auth_forms.UserCreationForm):
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = UserModel
+        fields = ['username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'gender']
 
 
-class LoginForm(AuthenticationForm):
+class LoginForm(auth_forms.AuthenticationForm):
     remember_me = forms.BooleanField(
         required=False,
         initial=True,
--- Fitness_store/Fitness_store/fitness_app/views.py
+++ Fitness_store/Fitness_store/fitness_app/views.py
@@ -57,12 +57,21 @@ class SupplementProductPageView(views.DetailView):
 class RegisterUserView(views.CreateView):
     template_name = 'register.html'
     form_class = RegisterUserForm
-    success_url = reverse_lazy('')
+    success_url = reverse_lazy('homepage')
 
     def form_valid(self, form):
         result = super().form_valid(form)
-        login(self.request, self.object)
+        user = self.object
+        login(self.request, user)
         return result
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', '')
+        return context
+
+    def get_success_url(self):
+        return self.request.POST.get('next', self.success_url)
 
 
 class LoginUserView(auth_views.LoginView):
