d4411bf1c26265c86f4ba4ea080a14685387af22
Iliyan Galabov <milenov556@gmail.com>
2023-11-08 13:49:32 +0200

    ShippingAddress model added to the database

--- Fitness_store/Fitness_store/fitness_app/models.py
+++ Fitness_store/Fitness_store/fitness_app/models.py
@@ -177,3 +177,16 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.name}"
+
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(FitnessUser, on_delete=models.SET_NULL, blank=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    region = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.address
