a2fcee1a3e1c9743caa11f8d2976110b6a927176
Iliyan Galabov <milenov556@gmail.com>
2023-11-14 12:46:34 +0200

    get_or_create_order added to utils.py

--- Fitness_store/Fitness_store/fitness_app/utils.py
+++ Fitness_store/Fitness_store/fitness_app/utils.py
@@ -1,4 +1,4 @@
-from .models import Cart
+from .models import Cart, Order
 
 
 def get_or_create_cart(request):
@@ -13,3 +13,17 @@ def get_or_create_cart(request):
             request.session['cart_id'] = cart.id
 
     return cart
+
+
+def get_or_create_order(request):
+    if request.user.is_authenticated:
+        order, created = Order.objects.get_or_create(user=request.user)
+    else:
+        order_id = request.session.get('cart_id')
+        if order_id:
+            order, created = Order.objects.get_or_create(id=order_id)
+        else:
+            order = Order.objects.create()
+            request.session['order_id'] = order.id
+
+    return order
