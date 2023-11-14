c6e8dde10c1bde6c2a5a48ab97871ee2f4f9aa16
Iliyan Galabov <milenov556@gmail.com>
2023-11-14 15:23:51 +0200

    Update context_processors.py

--- Fitness_store/Fitness_store/fitness_app/context_processors.py
+++ Fitness_store/Fitness_store/fitness_app/context_processors.py
@@ -19,17 +19,16 @@ def get_product_price(product_type, product_id):
 def cart_context(request):
     cart_items = []
     cart_total = 0
-    cart_in_progress = False
 
     if request.user.is_authenticated:
         cart = Cart.objects.filter(user=request.user).first()
 
         if cart:
-            cart_items = cart.cartitem_set.filter(in_progress=False)
-            cart_total = sum(item.price * item.quantity for item in cart_items if item.in_progress is False)
+            cart_items = cart.cartitem_set.all()
+            cart_total = sum(item.price * item.quantity for item in cart_items)
 
     elif 'cart_id' in request.session:
-        cart_data = Cart.objects.get(id=request.session.get('cart_id')).cartitem_set.filter(in_progress=False)
+        cart_data = Cart.objects.get(id=request.session.get('cart_id')).cartitem_set.all()
         cart_items = cart_data
         cart_total = sum(item.price * item.quantity for item in cart_items if item.in_progress is False)
 
