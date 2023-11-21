089f843eea140d1d6bdaea3a9950e4cdc0251403
Iliyan Galabov <milenov556@gmail.com>
2023-11-21 12:35:22 +0200

    signal for profile picture deletion added

--- Fitness_store/Fitness_store/fitness_app/apps.py
+++ Fitness_store/Fitness_store/fitness_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FitnessAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Fitness_store.fitness_app'
+
+    def ready(self):
+        import Fitness_store.fitness_app.signals
