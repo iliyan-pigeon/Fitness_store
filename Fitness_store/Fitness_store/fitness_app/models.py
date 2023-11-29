c90ba850a6fd2609426ce2d7b8367340ab337ac2
Iliyan Galabov <milenov556@gmail.com>
2023-11-29 16:35:36 +0200

    payment field added to the Order model

--- Fitness_store/Fitness_store/fitness_app/models.py
+++ Fitness_store/Fitness_store/fitness_app/models.py
@@ -202,6 +202,7 @@ class Order(models.Model):
     zipcode = models.CharField(max_length=200, null=True)
     date_added = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    payment = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return f"{self.address}"
