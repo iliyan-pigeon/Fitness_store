84a59c3a679bb348e76e0fab14c59205ee8d2184
Iliyan Galabov <milenov556@gmail.com>
2023-12-06 10:45:43 +0200

    Added all tests for all fields for Cart model

--- Fitness_store/tests/fitness_app/models/cart_test.py
+++ Fitness_store/tests/fitness_app/models/cart_test.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from Fitness_store.fitness_app.models import Cart, FitnessUser
 from django.utils import timezone
@@ -14,4 +15,26 @@ class CartTest(TestCase):
             'session_key': 'test_session_key'
         }
         self.cart = Cart.objects.create(**self.VALID_DATA)
-        
+
+    def test_valid_cart_creation(self):
+        self.assertEqual(self.cart.user, self.valid_user)
+        self.assertIsNotNone(self.cart.created_at)
+        self.assertEqual(self.cart.session_key, 'test_session_key')
+
+    def test_when_session_key_is_longer_than_max_length(self):
+        self.cart.session_key = 'a' * (self.SESSION_KEY_MAX_LENGTH + 1)
+
+        with self.assertRaises(ValidationError) as ve:
+            self.cart.full_clean()
+
+        expected_error_message = {'session_key': [f'Ensure this value has at most {self.SESSION_KEY_MAX_LENGTH}'
+                                                  f' characters (it has {self.SESSION_KEY_MAX_LENGTH+1}).']}
+        self.assertEqual(expected_error_message, ve.exception.message_dict)
+
+    def test_str_representation_with_user(self):
+
+        self.assertEqual(str(self.cart), f"Cart for testuser")
+
+    def test_str_representation_without_user(self):
+        self.cart.user = None
+        self.assertEqual(str(self.cart), f"Guest Cart")
