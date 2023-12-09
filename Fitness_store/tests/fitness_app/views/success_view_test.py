d08d6d827d70a2fd59bfabfcb2201870700b11f4
Iliyan Galabov <milenov556@gmail.com>
2023-12-09 19:27:03 +0200

    Update success_view_test.py

--- Fitness_store/tests/fitness_app/views/success_view_test.py
+++ Fitness_store/tests/fitness_app/views/success_view_test.py
@@ -3,11 +3,10 @@ from django.urls import reverse
 
 
 class SuccessViewTest(TestCase):
+
     def test_success_view(self):
-        success_url = reverse('success')  # Assuming you have a URL configuration for the success view.
+        success_url = reverse('success')
         response = self.client.get(success_url)
 
-        # Assuming the view renders a specific template.
         self.assertTemplateUsed(response, 'success.html')
 
-        # Add more assertions as needed based on your view's behavior and context.
